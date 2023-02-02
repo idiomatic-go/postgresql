918b10b14c8b2fece771f569742cb7b7e95be319
idiomatic-go <markbwilson@outlook.com>
2023-02-02 15:21:33 -0600

    Update error.go

--- pgxsql/error.go
+++ pgxsql/error.go
@@ -6,14 +6,31 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
+/*
+code
+message
+detail
+hint
+position
+internalPosition
+internalQuery
+where
+SchemaName
+TableName
+ColumnName
+DataTypeName
+ConstraintName
+
+*/
 func recast(err error) error {
 	if err == nil {
 		return err
 	}
 	var pgErr *pgconn.PgError
 	if errors.As(err, &pgErr) {
-		msg := fmt.Sprintf("code:%v, message:%v, position:%v, line:%v",
-			pgErr.Code, pgErr.Message, pgErr.InternalPosition, pgErr.Line)
+		msg := fmt.Sprintf("serverity:%v, code:%v, message:%v, position:%v, SQLState:%v",
+			pgErr.Severity, pgErr.Code, pgErr.Message, pgErr.Position, pgErr.SQLState())
+		//, pgErr.Line)
 		//pgErr.Code,pgErr.ColumnName,pgErr.ConstraintName,pgErr.DataTypeName,pgErr.Message,
 		//pgErr.Detail,pgErr.File,pgErr.Hint,pgErr.InternalPosition,pgErr.Line
 		return errors.New(msg)
