3f46153f20a94e7acf5fcc7b600e7f915dfb3e84
idiomatic-go <markbwilson@outlook.com>
2023-02-02 08:13:08 -0600

    Create scan.go

---
+++ pgxsql/scan.go
@@ -0,0 +1,32 @@
+package pgxsql
+
+import (
+	"errors"
+)
+
+type Scanner[T any] interface {
+	Scan(values []any) T
+}
+
+func Scan[T Scanner[T]](rows Rows) ([]T, error) {
+	if rows == nil {
+		return nil, errors.New("invalid request: rows interface is nil")
+	}
+	var s T
+	var t []T
+	var err error
+	var values []any
+
+	for rows.Next() {
+		err = rows.Err()
+		if err != nil {
+			return t, err
+		}
+		values, err = rows.Values()
+		if err != nil {
+			return t, err
+		}
+		t = append(t, s.Scan(values))
+	}
+	return t, nil
+}
